526cc72b91858e83109c220e488670b5efac4884
Florian Dubois <floriandubois115@gmail.com>
2021-08-25 18:56:17 +0200

    Improve password/gamemode selection handling

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 # Password check
 import lib, importlib
 
-password = input("Enter the password to continue: ")
+def checkPassword():
+	value = input("Enter the password to continue: ")
 
-if not lib.tryNumber(password):
-	print("Password is invalid.")
-	exit()
+	if not lib.tryNumber(value):
+		print("Password is invalid.")
+		checkPassword();
+		return
 
-if int(password) != 27412:
-	print("Password is incorrect.")
-	exit()
+	if int(value) != 27412:
+		print("Password is incorrect.")
+		checkPassword();
+		return
+
+checkPassword()
 
 # Selection of the gamemode
 modes = ["RPG [WIP]", "Auto-Development [Unavailable]", "Debug/Test [OK]"]
 
-print("Which mode do you want to use now?")
+def selectGamemode():
+	print("Which gamemode do you want to use now?")
 
-for index in range(len(modes)):
-	print(str(index + 1) + "-", modes[index])
+	for index in range(len(modes)):
+		print(str(index + 1) + "-", modes[index])
 
-selection = input()
+	selection = input()
 
-if not lib.tryNumber(selection):
-	print("Error!")
+	if not lib.tryNumber(selection):
+		print("Error!")
+		selectGamemode()
+		return
 
-selection = int(selection)
+	selection = int(selection)
 
-if selection == 1:
-	importlib.import_module("game")
-elif selection == 2:
-	pass # NYI
-elif selection == 3:
-	importlib.import_module("test")
-else:
-	print("Impossible to find the gamemode.")
-	exit()
+	if selection == 1:
+		importlib.import_module("game")
+	elif selection == 2:
+		print("Not Yet Implemented")
+		pass # NYI
+	elif selection == 3:
+		importlib.import_module("test")
+	else:
+		print("Unable to find the gamemode.")
+		selectGamemode()
+
+selectGamemode()
