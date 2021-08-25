1d376fc09daacfd0a8cb0bbbdbdacfca87c8ad8c
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-25 13:30:57 +0200

    Test/Debug file

---
+++ test.py
@@ -0,0 +1,25 @@
+# "If" statements
+x = int(input("Please enter a number: "))
+
+if x == 0:
+	print("test")
+elif x > 0:
+	print("loser!!!")
+
+# Iterate through lists
+words = ["test1", "test2", "test3"]
+
+for value in words:
+	print(value, len(value))
+
+# Functions
+def test(value = "test"):
+	print(value)
+
+test("hello world!")
+
+# Import system
+import lib
+
+test(lib.tryNumber("hey"))
+test(lib.tryNumber("123"))
