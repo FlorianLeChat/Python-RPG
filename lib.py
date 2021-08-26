b44b6ce2437d01592ef1ba5bd00083cf0334a869
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-26 11:25:24 +0200

    Library functions improvements

--- lib.py
+++ lib.py
@@ -1,11 +1,12 @@
 # Try to check if the input is a number
-# Source : https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+# https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
 def tryNumber(value):
-    try:
-        int(value)
-        return True
-    except ValueError:
-        return False
+	try:
+		int(value)
+		return True
+	except ValueError:
+		return False
+
 # Try to retrieve the input from the usage by checking the termination
 # controls like CTRL+C to stop the program.
 def getInput(prompt = ""):
@@ -15,15 +16,15 @@ def getInput(prompt = ""):
 		return False
 
 # Returns a list of files inside a single folder.
-# Source : https://stackoverflow.com/questions/1724693/find-a-file-in-python
+# https://stackoverflow.com/questions/1724693/find-a-file-in-python
 import os, fnmatch
 
 def findFiles(pattern, path):
-    result = []
+	result = []
 
-    for root, _, files in os.walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, pattern):
-                result.append(os.path.join(root, name))
+	for root, _, files in os.walk(path):
+		for name in files:
+			if fnmatch.fnmatch(name, pattern):
+				result.append(os.path.join(root, name))
 
-    return result
+	return result
