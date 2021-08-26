96f808d673c793a46497dde0fe0dcab44d812e9a
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-26 15:33:06 +0200

    Comments and game name improvements

--- game.py
+++ game.py
@@ -1,62 +1,70 @@
-# Game selection
+# Story selection
 from pathlib import Path
-import lib, json
+import lib, json, time
 
-games = lib.findFiles("*.json", "./universes")
+games = lib.findFiles("*.json", "./stories")
 titles = []
 
 for game in games:
 	titles.append(Path(game).stem.capitalize())
 
-def selectGame():
+def selectStory():
 	lib.consoleLog(message = "Welcome to the text-based storytelling experience.")
 	lib.consoleLog(message = "To begin, please write that you want to play.")
 
+	# Iterating through all the stories.
 	for index in range(len(titles)):
 		lib.consoleLog(message = str(index + 1) + "- " + titles[index])
 
 	name = lib.tryGetInput()
 
+	# Checks if the input is valid.
 	if not name:
 		lib.consoleLog(prefix = "Error", message = "Invalid input.")
-		selectGame()
+		selectStory()
 		return
 
 	name = name.capitalize()
 
+	# Checks if the input refers to one of the titles in memory.
 	for title in titles:
 		if name.find(title) != -1:
-			loadGame(title)
+			loadStory(title)
 			return
 
-	lib.consoleLog(prefix = "Error", message = "Unable to find the game \"" + name + "\".")
-	selectGame()
+	lib.consoleLog(prefix = "Error", message = "Unable to find the story \"" + name + "\".")
+	selectStory()
 
-# Loading game data
-def loadGame(name):
-	lib.consoleLog(message = "Do you want to load the game: \"" + name + "\"? (Y/N) ", newLine = "")
+# Loading story data
+def loadStory(name):
+	lib.consoleLog(message = "Do you want to load the story: \"" + name + "\"? (Y/N) ", newLine = "")
 
+	# Checks if the input is valid.
 	confirmation = lib.tryGetInput()
 
 	if not confirmation:
 		lib.consoleLog(prefix = "Error", message = "Invalid input.")
-		loadGame(name)
+		loadStory(name)
 		return
 
 	confirmation = confirmation.upper()
 
+	# Checks if the input means "yes" or "no".
+	# Repeats until it has a correct value.
 	if not confirmation or (confirmation != "Y" and confirmation != "N"):
-		loadGame(name)
+		loadStory(name)
 		return
 
+	# Check if the answer is negative. The loading is cancelled.
 	if confirmation.strip() == "N":
-		selectGame()
+		selectStory()
 		return
 
 	name = name.lower()
 
 	lib.consoleLog(message = "Loading data...")
 
+	# Load the corresponding story file.
 	file = open("./universes/" + name + ".json", "r", encoding = "utf-8")
 	data = json.loads(file.read())
 
@@ -64,9 +72,10 @@ def loadGame(name):
 
 	if not info or not script:
 		lib.consoleLog(prefix = "Error", message = "Game data error.")
-		selectGame()
+		selectStory()
 		return
 
+	# Displays some key information about the story.
 	print()
 	print("~ " + info.get("title", "@Title") + " ~")
 	print("By " + info.get("author", "@Author"))
@@ -79,3 +88,4 @@ def loadGame(name):
 
 	file.close()
 
+loadGame("Cuban")
