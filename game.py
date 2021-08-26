1f914d9302cb8d6dde2b829072affe666bb85742
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-26 11:29:31 +0200

    Basic game loader

---
+++ game.py
@@ -0,0 +1,31 @@
+# Game selection
+from pathlib import Path
+import lib
+
+games = lib.findFiles("*.json", "./universes")
+titles = []
+
+for game in games:
+	titles.append(Path(game).stem.capitalize())
+
+def selectGame():
+	print("Welcome to the text-based storytelling experience.")
+	print("To begin, please write that you want to play.")
+
+	for index in range(len(titles)):
+		print(str(index + 1) + "-", titles[index])
+
+	name = lib.getInput()
+
+	if not name:
+		print("Invalid input.")
+		selectGame()
+		return
+
+	for title in titles:
+		if name.find(title.lower()) != -1:
+			#loadGame
+			break
+
+	print("Impossible to find the game \"" + name + "\". Please try again.")
+	selectGame()
