787e3c9cd76e78d5f482727a430c4a1a4258cd49
Florian Dubois <floriandubois115@gmail.com>
2021-08-26 22:26:12 +0200

    Add a cooldown between each field reading

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ DEBUG_MODE = True
 # Time between each word display when reading a story.
 # (In milliseconds, implicit conversion)
 FADE_TIME = 25 / 1000
+
+# Time between each field reading (in seconds).
+WAIT_TIME = 1
