14916045c5bb9b0e4b14a72d437f18e2e2235bfa
Florian Dubois <floriandubois115@gmail.com>
2021-08-26 15:17:37 +0200

    Remove "[RPG]" prefix in console messages

--- lib.py
+++ lib.py
@@ -17,7 +17,7 @@ def tryGetInput(prompt = ""):
 
 # Displays a message in the output terminal formatted for the RPG project.
 def consoleLog(prefix = "Info", message = "", newLine = "\n"):
-	print("[RPG-" + prefix + "] " + message, end = newLine)
+	print("[" + prefix + "] " + message, end = newLine)
 
 # Returns a list of files inside a single folder.
 # https://stackoverflow.com/questions/1724693/find-a-file-in-python
