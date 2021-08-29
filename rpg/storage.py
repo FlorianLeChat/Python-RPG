d939afe95f70a703956654ac1caa5cb0f44a0a62
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-08-29 19:52:20 +0200

    Add the saving of the internal file when adding a value in the cache

--- rpg/storage.py
+++ rpg/storage.py
@@ -4,13 +4,33 @@
 from pathlib import Path
 import json, settings, lib
 
-file = Path("./rpg/data/__internal__.json")
 cache = {}
 
-if file.exists():
-	# Loads the save data if it exists.
-	file = file.open(encoding = "utf-8")
-	cache = json.loads(file.read())
+def saveFile():
+	file = Path(settings.SAVE_FILE_NAME)
+
+	if file.exists():
+		# Opens the file in write mode before converting the
+		# JSON string into a Python object.
+		file = file.open(mode = "w", encoding = "utf-8")
+
+		global cache
+		json.dump(cache, file, ensure_ascii = False, indent = 4)
+
+		file.close()
+
+def loadFile():
+	file = Path(settings.SAVE_FILE_NAME)
+
+	if file.exists():
+		# Opens the file in read mode before converting the
+		# cache to a JSON string for saving.
+		file = file.open(mode = "r", encoding = "utf-8")
+
+		global cache
+		cache = json.loads(file.read())
+
+		file.close()
 
 def debugLog(prefix = "Error", args = ""):
 	# Only in debug mode.
@@ -22,20 +42,27 @@ def saveData(name, field, key = "none", value = ""):
 	data = cache.get(name)
 
 	if not data:
+		debugLog(args = "Missing story \"" + name + "\" in cache, creating...")
 		cache[name] = {}
 
 	# Checks if the field is valid.
 	data = cache[name].get(field)
 
 	if not data:
+		debugLog(args = "Missing field \"" + field + "\" in cache, creating...")
 		cache[name][field] = {}
 
 	# Checks if the value requires a key to be saved
 	# or directly a save in the field.
 	if key != "none":
+		debugLog(args = "Missing key \"" + key + "\" in cache, creating...")
 		cache[name][field][key] = value
 	else:
 		cache[name][field] = value
+
+	# Finally saves the file after the transaction.
+	debugLog(prefix = "Info", args = "Cache file successfully saved.")
+	saveFile()
 
 def loadData(name, field, key = "none", fallback = ""):
 	# Loads the data and checks if they are valid.
@@ -56,3 +83,8 @@ def loadData(name, field, key = "none", fallback = ""):
 
 	# Returns the value in the field key or its name.
 	return key != "none" and index.get(key, fallback) or index
+
+#
+# End of file
+#
+loadFile()
